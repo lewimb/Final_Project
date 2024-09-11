ea4283c8f3af8a87da47c421a9d1914570f6b398
Lewi Meyvianus Borosi <lewiyeeey@gmail.com>
2024-09-11 20:30:44 +0700

    new progress

--- Fe/app/page.tsx
+++ Fe/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import Register from "@/components/Register";
+import "./globals.css";
 
 export default function Home() {
   return (
     <>
-      <Register />
+      <h1 className="bg-black">Lewi</h1>
+      <Navbar />
     </>
   );
 }
