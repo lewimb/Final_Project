e8e0e2021b95ed8123473fc3f21dccee64eb4b37
hrndbrs <hrndbrs@gmail.com>
2024-09-18 01:30:48 +0700

    refactor button and card components

--- Fe/app/(main)/page.tsx
+++ Fe/app/(main)/page.tsx
@@ -1,11 +1,44 @@
-import Card from "@/components/card";
+import Card, { Item } from "@/components/card";
 import Carousel from "@/components/carousel";
 
 export default async function Home() {
   return (
     <>
       <Carousel />
-      <Card />
+
+      <div className="bg-gray-900">
+        <h1 className="card-title drop-shadow-2xl text-2xl text-center pt-5 mx-3 font-semibold text-white">
+          Search by item
+        </h1>
+        <div>
+          <div className="grid grid-cols-3 gap-11 p-10 justify-between ">
+            {items.map((item) => (
+              <Card key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
+
+let items: Item[] = [
+  {
+    id: 1,
+    name: "Item 1",
+    img: "/assets/carousel-placeholder.avif",
+    description: "This is the description for Item 1.",
+  },
+  {
+    id: 2,
+    name: "Item 2",
+    img: "/assets/carousel-placeholder.avif",
+    description: "This is the description for Item 2.",
+  },
+  {
+    id: 3,
+    name: "Item 3",
+    img: "/assets/carousel-placeholder.avif",
+    description: "This is the description for Item 3.",
+  },
+];
--- Fe/components/card.tsx
+++ Fe/components/card.tsx
@@ -1,62 +1,26 @@
-"use client";
-import { useState } from "react";
 import Image from "next/image";
 
-type Items = {
+export type Item = {
   id: number;
   name: string;
   img: string;
   description: string;
 };
 
-let items: Items[] = [
-  {
-    id: 1,
-    name: "Item 1",
-    img: "/assets/carousel-placeholder.avif",
-    description: "This is the description for Item 1.",
-  },
-  {
-    id: 2,
-    name: "Item 2",
-    img: "/assets/carousel-placeholder.avif",
-    description: "This is the description for Item 2.",
-  },
-  {
-    id: 3,
-    name: "Item 3",
-    img: "/assets/carousel-placeholder.avif",
-    description: "This is the description for Item 3.",
-  },
-];
-
-export default function Card() {
+export default function Card({ item }: { item: Item }) {
   return (
-    <>
-      <div className="bg-gray-900">
-        <h1 className="card-title drop-shadow-2xl text-2xl text-center pt-5 mx-3 font-semibold text-white">
-          Search by item
-        </h1>
-        <div>
-          <div className="grid grid-cols-3 gap-11 p-10 justify-between ">
-            {items.map((value, index) => (
-              <div key={value.id} className="card flex">
-                <div>
-                  <Image
-                    src={value.img}
-                    alt="baju"
-                    width={500}
-                    height={500}
-                    className="item rounded-xl"
-                  ></Image>
-                  <h1 className="text-center py-3 font-bold">{value.name}</h1>
-                  <p className="text-center">{value.description}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+    <div className="card flex">
+      <div>
+        <Image
+          src={item.img}
+          alt="baju"
+          width={500}
+          height={500}
+          className="item rounded-xl"
+        ></Image>
+        <h1 className="text-center py-3 font-bold">{item.name}</h1>
+        <p className="text-center">{item.description}</p>
       </div>
-    </>
+    </div>
   );
 }
